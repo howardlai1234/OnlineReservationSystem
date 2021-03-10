48dd269c804f41101221ca2b30a7df0739ebfdcc
Howard Lai <howrdlai1234@gmail.com>
2021-03-11 01:59:25 +0800

    added database for message

--- message/views.py
+++ message/views.py
@@ -3,10 +3,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'message.html', {
-            'username':request.session['username'],
+    if 'userid' in request.session:
+        db_conn = connections['default']
+        cursor = db_conn.cursor()
+        sql ='SELECT messageID, sendTime, title, bodyw FROM message WHERE receiverID="' + str(userid) + '" ORDER BY messageID'
+        cursor.execute(sql)
+        row = cursor.fetchall()
 
-            })
+        return render(request, 'message.html', {
+                'username':request.session['username'],
+
+                })
+    else:
+        return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>')
 
 
 
