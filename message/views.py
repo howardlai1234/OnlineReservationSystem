edc8e2511d22ab8a81031b9763d484bff95d8de8
Howard Lai <howrdlai1234@gmail.com>
2021-03-19 20:33:24 +0800

    commented old code

--- message/views.py
+++ message/views.py
@@ -7,32 +7,38 @@ from django.db.utils import OperationalError
 
 
 def home(request):
-    if 'userid' in request.session:
-        userid = request.session['userid']
-        db_conn = connections['default']
-        cursor = db_conn.cursor()
-        sql = 'SELECT messageID, sendTime, title, body FROM message WHERE receiverID="' + \
-            str(userid) + '" ORDER BY messageID'
-        cursor.execute(sql)
-        row = cursor.fetchall()
 
-        return render(request, 'message.html', {
-            'username': request.session['username'],
 
-        })
-    else:
-        return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>')
+    ##the following code is for older version which uses RAW SQL
+
+    # if 'userid' in request.session:
+    #     userid = request.session['userid']
+    #     db_conn = connections['default']
+    #     cursor = db_conn.cursor()
+    #     sql = 'SELECT messageID, sendTime, title, body FROM message WHERE receiverID="' + \
+    #         str(userid) + '" ORDER BY messageID'
+    #     cursor.execute(sql)
+    #     row = cursor.fetchall()
+
+    #     return render(request, 'message.html', {
+    #         'username': request.session['username'],
+
+    #     })
+    # else:
+    #     return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>')
 
 
 def view(request):
-    if 'userid' in request.session:
-        if request.GET != '':
-            print("get:", request.GET)
-            return HttpResponse('ok')
-        else:
-            return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Incorrect or messing messageID <br> <br><a href="/message">Back</a>')
-    else:
-        return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>')
+
+    ##the following code is for older version which uses RAW SQL
+    # if 'userid' in request.session:
+    #     if request.GET != '':
+    #         print("get:", request.GET)
+    #         return HttpResponse('ok')
+    #     else:
+    #         return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Incorrect or messing messageID <br> <br><a href="/message">Back</a>')
+    # else:
+    #     return HttpResponse('<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>')
 
 
 def create_new(request):
