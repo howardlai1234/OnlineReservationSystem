1bc67284c600b6e27b9239efb829c55fbfcc697a
Howard Lai <howrdlai1234@gmail.com>
2021-03-26 03:10:29 +0800

    Added to view available slots in html
    ref: #8

--- slotSelect/views.py
+++ slotSelect/views.py
@@ -3,25 +3,46 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
 from ORS.settings import DEBUG
 from config.models import Currentphase, Timetable
+from dashboard.models import Slot
 # Create your views here.
 
 def home(request):
-if request.user.is_authenticated:
-    phase = Currentphase.objects.get().phase
-    allowed_group = Timetable.objects.get().phase1_group_name
-    userid = User.objects.get(username=request.user).pk
-    group = User.objects.get(username=request.user).groups
+    if request.user.is_authenticated:
+        phase = Currentphase.objects.get().phase
+        allowed_group = Timetable.objects.get().phase2_group_name
+        userid = User.objects.get(username=request.user).pk
+        group = User.objects.get(username=request.user).groups
+        user_allowed_to_access = False
+        RegisteredSlotsReturn = []
+        grouplist = []
 
-    if DEBUG == True:
-        phase = 2
+        if DEBUG == True:
+            phase = 2
 
-    if phase == 2:
-        
+        if phase == 2:
+            for gp in request.user.groups.all():
+                grouplist.append(gp.name)
+                if gp.name == allowed_group:
+                    user_allowed_to_access = True
+            if user_allowed_to_access == True:
+                #request all available slots
+                for gp in request.user.groups.all():
+                    if Slot.objects.filter(groupid=Group.objects.get(name=gp.name).pk).count() > 0:
+                        Registered_slot_of_group = []
+                        for s in Slot.objects.filter(groupid=Group.objects.get(name=gp.name).pk).all():
+                            Registered_slot_of_group.append({'id': s.slotid, 'start': s.starttime, 'end': s.endtime})
+                        RegisteredSlotsReturn.append({'group': gp.name, 'slots': Registered_slot_of_group})
 
-        return HttpResponse("welcome")
+
+                return render(request, 'selection.html', {
+                    'availableTimes': RegisteredSlotsReturn,
+                })
+            else:
+                return HttpResponse(
+                    '<h1>ACCEESS DENIED</h1> <br> You are not allowed to be here, please contact an administrator if you think you should <br> <br><a href="/dashboard">return</a>', status=403)
+        else:
+            return HttpResponse(
+                '<h1>ACCEESS DENIED</h1> ', status=403)
     else:
         return HttpResponse(
-            '<h1>ACCEESS DENIED</h1> <br> You are not allowed to be here, please contact an administrator if you think you should <br> <br><a href="/dashboard">return</a>', status=403)
-else:
-    return HttpResponse(
-        '<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>', status=401)
+            '<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>', status=401)
