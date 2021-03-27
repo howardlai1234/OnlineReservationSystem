aa63314a9344c235ec0e3d40fa75004815445128
Howard Lai <howardlai1234@gmail.com>
2021-03-28 01:20:52 +0800

    fix formating
    ref: #18

--- slotSelect/models.py
+++ slotSelect/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Selection(models.Model):
     groupid = models.IntegerField()
     userid = models.IntegerField()
--- slotSelect/views.py
+++ slotSelect/views.py
@@ -11,30 +11,28 @@ from slotSelect.models import Selection
 
 def home(request):
     if request.user.is_authenticated:
-        #variable for Access control check
+        # variable for Access control check
         phase = Currentphase.objects.get().phase
         allowed_group = Timetable.objects.get().phase2_group_name
         userid = User.objects.get(username=request.user).pk
         group = User.objects.get(username=request.user).groups
         user_allowed_to_access = False
-        
 
-        #variable for generating slot list
+        # variable for generating slot list
         RegisteredSlotsReturn = []
         grouplist = ['All']
         cur_group = 'All'
 
-        #for checking previous submit selection of user
+        # for checking previous submit selection of user
         user_already_selected = {'flag': False}
 
-        #SelectionForm return message
+        # SelectionForm return message
         formError = ''
         formSuccess = ''
         failedSubmission = {'flag': False}
 
         if 'cur_group' in request.session:
             cur_group = request.session['cur_group']
-
 
         if DEBUG == True:
             phase = 2
@@ -50,7 +48,6 @@ def home(request):
 
             # Reserve for viewiing all group slots
 
-
             if request.method == "POST":
                 if 'group_select' in request.POST:
                     form = GroupSelectForm(request.POST)
@@ -59,14 +56,14 @@ def home(request):
                         cur_group = form.cleaned_data['groupselect']
                         request.session['cur_group'] = form.cleaned_data['groupselect']
 
-                        #temp. move here to prevent bug
-                        
+                        # temp. move here to prevent bug
+
                 if 'slot_select' in request.POST:
                     form = SlotSelectForm(request.POST)
                     if form.is_valid():
                         valid = True
 
-                        #split the form into list
+                        # split the form into list
                         SelectList_reurn = form.cleaned_data['selectionlist']
                         # SelectList_reurn = ''.join(SelectList_reurn.split())
                         # SelectList_reurn = SelectList_reurn.replace('\r', '')
@@ -78,7 +75,7 @@ def home(request):
                             if valid:
                                 try:
                                     int(t)
-                                except:
+                                except BaseException:
                                     formError = "Error: One of the selected item cannot be recognised"
                                     valid = False
 
@@ -86,13 +83,15 @@ def home(request):
                         if valid:
                             for i in range(0, len(slotSelectList)):
                                 for x in range(i + 1, len(slotSelectList)):
-                                    if int(slotSelectList[i]) == int(slotSelectList[x]):
+                                    if int(slotSelectList[i]) == int(
+                                            slotSelectList[x]):
                                         valid = False
                                         formError = "Error: One item is repeated"
-                        
-                        #check if all slot are balid for this group
+
+                        # check if all slot are balid for this group
                         if valid:
-                            allowed_slotID_for_this_group = Slot.objects.filter(groupid=Group.objects.get(name=form.cleaned_data['group']).pk).all()
+                            allowed_slotID_for_this_group = Slot.objects.filter(
+                                groupid=Group.objects.get(name=form.cleaned_data['group']).pk).all()
                             for i in slotSelectList:
                                 slotID_exist_in_this_group = False
                                 for s in allowed_slotID_for_this_group:
@@ -102,18 +101,24 @@ def home(request):
                                     valid = False
                                     formError = "ERROR: At lease one of the slotID is invalid"
 
-                        #store it in DB if all check passed
+                        # store it in DB if all check passed
                         if valid:
-                            groupid = Group.objects.get(name=form.cleaned_data['group']).pk
-                            print("before insert:",groupid, "name: ", form.cleaned_data['group'])
-                            Selection.objects.filter(groupid=groupid, userid=userid).delete()
+                            groupid = Group.objects.get(
+                                name=form.cleaned_data['group']).pk
+                            print(
+                                "before insert:",
+                                groupid,
+                                "name: ",
+                                form.cleaned_data['group'])
+                            Selection.objects.filter(
+                                groupid=groupid, userid=userid).delete()
                             for i in range(0, len(slotSelectList)):
                                 Selection.objects.create(
-                                    groupid = groupid,
-                                    userid = userid,
-                                    slotid = slotSelectList[i],
-                                    userorder = i + 1,
-                                    weightedscore = -1
+                                    groupid=groupid,
+                                    userid=userid,
+                                    slotid=slotSelectList[i],
+                                    userorder=i + 1,
+                                    weightedscore=-1
                                 )
                             formSuccess = "SUCCESS: your list is Saved"
                         else:
@@ -123,19 +128,23 @@ def home(request):
                         if DEBUG == True:
                             print("Selection Form Valid")
                             print('slotSelectList: ', slotSelectList)
-            
-            #generate the timetable list
-            
+
+            # generate the timetable list
+
             if cur_group == 'All':
                 for gp in request.user.groups.all():
-                    print("GP: ", gp, "ID",Group.objects.get(name=gp.name).pk)
-                    if Slot.objects.filter(groupid=Group.objects.get(name=gp.name).pk).count() > 0:
+                    print("GP: ", gp, "ID", Group.objects.get(name=gp.name).pk)
+                    if Slot.objects.filter(groupid=Group.objects.get(
+                            name=gp.name).pk).count() > 0:
                         Registered_slot_of_group = []
-                        for s in Slot.objects.filter(groupid=Group.objects.get(name=gp.name).pk).all():
-                            Registered_slot_of_group.append({'id': s.slotid, 'start': s.starttime, 'end': s.endtime})
-                        RegisteredSlotsReturn.append({'group': gp.name, 'slots': Registered_slot_of_group})
+                        for s in Slot.objects.filter(
+                                groupid=Group.objects.get(name=gp.name).pk).all():
+                            Registered_slot_of_group.append(
+                                {'id': s.slotid, 'start': s.starttime, 'end': s.endtime})
+                        RegisteredSlotsReturn.append(
+                            {'group': gp.name, 'slots': Registered_slot_of_group})
                 if DEBUG == True:
-                    print("RegisteredSlotsReturn: ",RegisteredSlotsReturn)
+                    print("RegisteredSlotsReturn: ", RegisteredSlotsReturn)
             else:
                 Registered_slot_of_group = []
                 groupid = Group.objects.get(name=cur_group).pk
@@ -144,23 +153,25 @@ def home(request):
                         {'id': s.slotid, 'start': s.starttime, 'end': s.endtime})
                 RegisteredSlotsReturn.append(
                     {'group': cur_group, 'slots': Registered_slot_of_group})
-                if 0 < Selection.objects.filter(groupid=groupid, userid=userid).count():
+                if 0 < Selection.objects.filter(
+                        groupid=groupid, userid=userid).count():
                     user_already_selected['flag'] = True
                     user_already_selected['list'] = []
-                    user_already_selected_list = Selection.objects.filter(groupid=groupid, userid=userid).order_by('userorder').all()
+                    user_already_selected_list = Selection.objects.filter(
+                        groupid=groupid, userid=userid).order_by('userorder').all()
                     for i in user_already_selected_list:
                         user_already_selected['list'].append(i.slotid)
 
             return render(request, 'selection.html', {
                 'formError': formError,
-                'formSuccess':  formSuccess,
+                'formSuccess': formSuccess,
                 'currentGroup': cur_group,
                 'grouplist': grouplist,
                 'availableTimes': RegisteredSlotsReturn,
                 'previousSelection': user_already_selected,
                 'failedSubmission': failedSubmission
             })
-                
+
         else:
             return HttpResponse(
                 '<h1>ACCEESS DENIED</h1> <br> You are not allowed to be here, please contact an administrator if you think you should <br> <br><a href="/dashboard">return</a>', status=403)
