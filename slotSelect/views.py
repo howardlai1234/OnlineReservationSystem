5488d24f99a3af96728ec7fbb67e70321cc443db
Howard Lai <howardlai1234@gmail.com>
2021-03-27 23:27:38 +0800

    Added checks for slot select
    ref: #18

--- slotSelect/views.py
+++ slotSelect/views.py
@@ -71,18 +71,49 @@ def home(request):
                     if 'slot_select' in request.POST:
                         form = SlotSelectForm(request.POST)
                         if form.is_valid():
-                            valid = 1
-                            slotSelectList = list(form.cleaned_data['selectionlist'].split(","))
+                            valid = True
+
+                            #split the form into list
+                            SelectList_reurn = form.cleaned_data['selectionlist']
+                            # SelectList_reurn = ''.join(SelectList_reurn.split())
+                            # SelectList_reurn = SelectList_reurn.replace('\r', '')
+                            # SelectList_reurn = SelectList_reurn.replace('\n', '')
+                            slotSelectList = list(SelectList_reurn.split(","))
+
+                            # check is all inputed value is integer
                             for t in slotSelectList:
-                                if valid == 1:
+                                if valid:
                                     try:
                                         int(t)
                                     except:
                                         formError = "Error: One of the selected item is not an integer"
-                            print("Selection Form Valid")
-                            print('slotSelectList: ', slotSelectList)
+                                        valid = False
 
+                            # check if ther is duplicated slot
+                            if valid:
+                                for i in range(0, len(slotSelectList)):
+                                    for x in range(i + 1, len(slotSelectList)):
+                                        if int(slotSelectList[i]) == int(slotSelectList[x]):
+                                            valid = False
+                                            formError = "Error: One item is repeated"
+                            
+                            #check if all slot are balid for this group
+                            allowed_slotID_for_this_group = Slot.objects.filter(groupid=Group.objects.get(name=form.cleaned_data['group']).pk).all()
+                            if valid:
+                                for i in slotSelectList:
+                                    slotID_exist_in_this_group = False
+                                    for s in allowed_slotID_for_this_group:
+                                        if int(i) == s.slotid:
+                                            slotID_exist_in_this_group = True
+                                    if slotID_exist_in_this_group == False:
+                                        valid = False
+                                        formError = "ERROR: At lease one of the slotID is invalid"
 
+                            if DEBUG == True:
+                                print("Selection Form Valid")
+                                print('slotSelectList: ', slotSelectList)
+                                if valid:
+                                    formSuccess = "ALL TEST PASSED"
                 return render(request, 'selection.html', {
                     'formError': formError,
                     'formSuccess':  formSuccess,
