d510adc7290d287efc39a8183ea4c6a340bbb353
Howard Lai <howardlai1234@gmail.com>
2021-03-27 23:33:29 +0800

    Added models for slot select
    Ref: #18

--- ORS/settings.py
+++ ORS/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'config',
     'message',
     'meeting',
+    'slotSelect',
 ]
 
 MIDDLEWARE = [
--- slotSelect/models.py
+++ slotSelect/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Selection(models.Model):
+    groupid = models.IntegerField()
+    userid = models.IntegerField()
+    userorder = models.IntegerField()
