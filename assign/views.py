d8990d7dee8f3459c676e7f04f875bb2597e8376
Howard Lai <howardlai1234@gmail.com>
2021-04-06 22:13:50 +0800

    Added access control
    Ref #25
    
    Added check that only admin can access the assign page

--- assign/views.py
+++ assign/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllow
 # Create your views here.
 
 def home(request):
+   if not request.user.is_authenticated:
+      return HttpResponse(
+        '<h1>ACCEESS DENIED</h1> <br> Please Login first <br> <br><a href="/login">Login</a>', status=401)
+   if not request.user.is_superuser:
+      return HttpResponse('<h1>ACCEESS DENIED</h1>', status=404)
+   
+
    return HttpResponse("Hello")
