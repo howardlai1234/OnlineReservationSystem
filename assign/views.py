38c71d2477efb81d1bda207769e540bb5a0a1845
Howard Lai <howardlai1234@gmail.com>
2021-04-07 23:22:03 +0800

    Remove unused testing code
    ref: #25

--- assign/views.py
+++ assign/views.py
@@ -87,27 +87,6 @@ def home(request):
                 print("slotlist: ", available_slot_lookup)
             batch_add_meeting(confirm_list)
 
-    # for usr in User.objects.all():
-    #    print ("username:", usr.username, "id:", User.objects.get(username=usr.username).pk)
-
-    # matrix = [[6, 4, 2, 25, 20, D, D, D],
-    #           [2, 20, D, D, 3, 25, 6, D],
-    #           [4, 2, 6, 25, D, D, D, 20],
-    #           [4, 6, 2, 25, 20, D, D, D],
-    #           [4, D, 6, D, 2, 20, 25, D],
-    #           [4, 2, 6, D, D, 25, 20, D]
-    #           ]
-    # m = Munkres()
-    # indexes = m.compute(matrix)
-    # print_matrix(matrix, msg='Lowest cost through this matrix:')
-    # total = 0
-    # print('index:', indexes)
-    # for row, column in indexes:
-    #    value = matrix[row][column]
-    #    total += value
-    #    print(f'({row}, {column}) -> {value}')
-    # print(f'total cost: {total}')
-
     return HttpResponse("Hello")
 
 
