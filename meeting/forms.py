e6d6f023fa0f4db11d8a0644c4cb25baf003760a
Howard Lai <howardlai1234@gmail.com>
2021-04-09 17:20:40 +0800

    Added form processing for manually create meeting
    Ref: #26

--- meeting/forms.py
+++ meeting/forms.py
@@ -8,3 +8,12 @@ class ManageForm(forms.Form):
     starttime = forms.TimeField()
     endtime = forms.TimeField()
     remark = forms.CharField()
+
+class CreateForm(forms.Form):
+    name = forms.CharField()
+    date = forms.DateField()
+    starttime = forms.TimeField()
+    endtime = forms.TimeField()
+    host = forms.CharField()
+    participant = forms.CharField()
+    remark = forms.CharField()
